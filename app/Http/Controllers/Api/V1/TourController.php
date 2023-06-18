1819aee12b462f59f0bcd8d84e0de0dce9f044ab
Dejan1990 <dejan.simo@hotmail.com>
2023-06-18 13:55:36 +0200

    Public Endpoint for Tours

---
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TourResource;
+use App\Models\Tour;
+use App\Models\Travel;
+use Illuminate\Http\Request;
+
+class TourController extends Controller
+{
+    public function index(Travel $travel)
+    {
+        //return Tour::where('travel_id', $travel->id)->orderBy('starting_date')->paginate();
+        $tours = $travel->tours()
+            ->orderBy('starting_date')
+            ->paginate();
+
+        return TourResource::collection($tours);
+    }
+}
