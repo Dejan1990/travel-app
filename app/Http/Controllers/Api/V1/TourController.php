eb3d332a92437b8b9712351ebcdbb5c395d42e19
Dejan1990 <dejan.simo@hotmail.com>
2023-06-18 16:03:05 +0200

    Tours filtering and ordering

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -10,10 +10,25 @@ use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel)
+    public function index(Travel $travel, Request $request)
     {
         //return Tour::where('travel_id', $travel->id)->orderBy('starting_date')->paginate();
         $tours = $travel->tours()
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('ending_date', '<=', $request->dateTo);
+            })
+            ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder);
+            })
             ->orderBy('starting_date')
             ->paginate();
 
