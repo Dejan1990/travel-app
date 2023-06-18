d1aa742d50a226db585b28ca13cee269144018f1
Dejan1990 <dejan.simo@hotmail.com>
2023-06-18 20:09:21 +0200

    Create user

--- app/Console/Commands/CreateUserCommand.php
+++ app/Console/Commands/CreateUserCommand.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUserCommand extends Command
 {
@@ -29,7 +31,7 @@ class CreateUserCommand extends Command
     {
         $user['name'] = $this->ask('Name of the new user');
         $user['email'] = $this->ask('Email of the new user');
-        $user['password'] = $this->secret('Password of the new user');
+        $user['password'] = Hash::make($this->secret('Password of the new user'));
 
         $roleName = $this->choice('Role of the new user', ['admin', 'editor'], 1);
 
@@ -40,7 +42,10 @@ class CreateUserCommand extends Command
             return -1;
         }
 
-        User::create($user);
+        DB::transaction(function () use ($user, $role) {
+            $newUser = User::create($user);
+            $newUser->roles()->attach($role->id);
+        });
 
         $this->info('User ' . $user['email'] . ' created successfully');
 
